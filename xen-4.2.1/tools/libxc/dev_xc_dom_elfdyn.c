98004876d1338d37a234f059d7d0a1dc25659caf
Cheng Wang2 <nle5255@N-1328.office.hd>
2018-07-30 17:14:25 +0200

    add dev_xc_dom_elfdyn func

---
+++ xen-4.2.1/tools/libxc/dev_xc_dom_elfdyn.c
@@ -0,0 +1,50 @@
+
+
+struct link_map {
+    Elf32_Addr l_addr;		/* Base (or delta?) domU address shared object is loaded at.  */
+    Elf32_Dyn *l_ld;		/* Dynamic section of the shared object.  */
+    Elf32_Half l_ldnum;		/* Number of dynamic segment entries.  */
+};
+
+typedef struct link_map map_t;
+
+int xc_dom_elf_dyn(struct xc_dom_image *dom)
+{
+	const elf_phdr *phdr;
+	Elf32_Phdr *ph;
+
+	uint32_t e_phnum;
+	uint32_t p_type, p_paddr, p_memsz, p_filesz, p_offset;
+
+	map_t *main_map = NULL;
+
+	struct elf_binary *elf = dom->private_loader;
+
+	e_phnum = elf_uval(elf, elf->ehdr, e_phnum);
+	/*Scan the program header table for the dynamic section.*/
+	for (i = 0; i < e_phnum; i++) {
+		phdr = elf_phdr_by_index(elf, i);
+		p_type = elf_uval(elf, phdr, p_type);
+		ph = (Elf32_Phdr *)phdr;
+		switch (p_type)
+		{
+			case PT_PHDR:
+				/*Find out the load address.*/
+				main_map->l_addr = dom->kernel_seg.vstart - dom->parms.virt_kstart;
+				DOMPRINTF("%s:main_map->l_addr = 0x%x", __FUNCTION__, main_map->l_addr);
+				break;
+			case PT_DYNAMIC:
+				p_paddr = elf_uval(elf, phdr, p_paddr);
+				p_offset = elf_uval(elf, phdr, p_offset);
+				p_filesz = elf_uval(elf, phdr, p_filesz);
+				p_memsz = elf_uval(elf, phdr, p_memsz);
+
+				main_map->l_ld = (Elf32_Dyn *)elf_get_ptr(elf, p_paddr);
+				main_map->l_ldnum = ph->p_memsz/sizeof(Elf32_Dyn);
+				DOMPRINTF("%s:l_ld = 0x%p, l_ldnum = %d", __FUNCTION__, main_map->l_ld, main_map->l_ldnum);
+				break;
+		}
+	}
+
+	return 0;
+}
---
+++ xen-4.2.1/tools/libxc/dev_xc_dom_elfdyn.h
@@ -0,0 +1,6 @@
+#ifndef DEV_XC_DOM_DYN_H
+#define DEV_XC_DOM_DYN_H
+
+int xc_dom_elf_dyn(struct xc_dom_image *dom);
+
+#endif //DEV_XC_DOM_DYN_H
