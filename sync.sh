a6fb453ca8f0753053053a7548e9fd72dba65f2a
Cheng Wang2 <nle5255@N-1328.office.hd>
2018-07-31 10:49:14 +0200

    sync

---
+++ sync.sh
@@ -0,0 +1,6 @@
+#bin/sh
+
+cp ~/xen/xen-4.2.1/tools/libxc/Makefile ~/xen-4.2.1/tools/libxc
+cp ~/xen/xen-4.2.1/tools/libxc/xc_dom_core.c ~/xen-4.2.1/tools/libxc
+cp ~/xen/xen-4.2.1/tools/dev_xc_dom_elfdyn.c ~/xen-4.2.1/tools/libxc
+cp ~/xen/xen-4.2.1/tools/dev_xc_dom_elfdyn.h ~/xen-4.2.1/tools/libxc
