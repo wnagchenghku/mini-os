f6c1ae1ac4d43fd05b34c0f11435501ff7bc405b
Cheng Wang2 <nle5255@N-1328.office.hd>
2018-08-09 16:42:45 +0200

    sync mk

--- sync.sh
+++ sync.sh
@@ -6,4 +6,5 @@ cp ~/xen/xen-4.2.1/tools/libxc/dev_xc_dom_elfdyn.c ~/xen-4.2.1/tools/libxc
 cp ~/xen/xen-4.2.1/tools/libxc/dev_xc_dom_elfdyn.h ~/xen-4.2.1/tools/libxc
 
 cp ~/xen/xen-4.2.1/extras/mini-os/Makefile ~/xen-4.2.1/extras/mini-os
+cp ~/xen/xen-4.2.1/extras/mini-os/minios.mk ~/xen-4.2.1/extras/mini-os
 cp ~/xen/xen-4.2.1/extras/mini-os/test.c ~/xen-4.2.1/extras/mini-os
