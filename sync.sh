4d22e6abca0e9dcf6304d107499f2ad6a69a2671
Your Name <you@example.com>
2018-08-13 14:11:48 +0200

    4.6.5 sync

--- sync.sh
+++ sync.sh
@@ -1,5 +1,16 @@
 #bin/sh
 
+cp ~/xen/xen-4.6.5/tools/libxc/Makefile ~/xen-4.6.5/tools/libxc
+cp ~/xen/xen-4.6.5/tools/libxc/xc_dom_core.c ~/xen-4.6.5/tools/libxc
+cp ~/xen/xen-4.6.5/tools/libxc/dev_xc_dom_elfdyn.c ~/xen-4.6.5/tools/libxc
+cp ~/xen/xen-4.6.5/tools/libxc/dev_xc_dom_elfdyn.h ~/xen-4.6.5/tools/libxc
+
+cp ~/xen/xen-4.6.5/extras/mini-os/Makefile ~/xen-4.6.5/extras/mini-os
+cp ~/xen/xen-4.6.5/extras/mini-os/minios.mk ~/xen-4.6.5/extras/mini-os
+cp ~/xen/xen-4.6.5/extras/mini-os/test.c ~/xen-4.6.5/extras/mini-os
+
+#4.2.1
+
 cp ~/xen/xen-4.2.1/tools/libxc/Makefile ~/xen-4.2.1/tools/libxc
 cp ~/xen/xen-4.2.1/tools/libxc/xc_dom_core.c ~/xen-4.2.1/tools/libxc
 cp ~/xen/xen-4.2.1/tools/libxc/dev_xc_dom_elfdyn.c ~/xen-4.2.1/tools/libxc
