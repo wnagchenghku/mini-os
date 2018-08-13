3ead570dafd9101250fda20e5a342f7bd285ffc7
Cheng Wang2 <nle5255@N-1328.office.hd>
2018-08-13 15:59:17 +0200

    sync.sh

--- sync.sh
+++ sync.sh
@@ -1,11 +1,11 @@
 #bin/sh
 #4.2.1
 
-cp ~/xen/xen-4.2.1/tools/libxc/Makefile ~/xen-4.2.1/tools/libxc
-cp ~/xen/xen-4.2.1/tools/libxc/xc_dom_core.c ~/xen-4.2.1/tools/libxc
-cp ~/xen/xen-4.2.1/tools/libxc/dev_xc_dom_elfdyn.c ~/xen-4.2.1/tools/libxc
-cp ~/xen/xen-4.2.1/tools/libxc/dev_xc_dom_elfdyn.h ~/xen-4.2.1/tools/libxc
+cp ~/xen/xen-4.2.1-amd64/tools/libxc/Makefile ~/xen-4.2.1/tools/libxc
+cp ~/xen/xen-4.2.1-amd64/tools/libxc/xc_dom_core.c ~/xen-4.2.1/tools/libxc
+cp ~/xen/xen-4.2.1-amd64/tools/libxc/dev_xc_dom_elfdyn.c ~/xen-4.2.1/tools/libxc
+cp ~/xen/xen-4.2.1-amd64/tools/libxc/dev_xc_dom_elfdyn.h ~/xen-4.2.1/tools/libxc
 
-cp ~/xen/xen-4.2.1/extras/mini-os/Makefile ~/xen-4.2.1/extras/mini-os
-cp ~/xen/xen-4.2.1/extras/mini-os/minios.mk ~/xen-4.2.1/extras/mini-os
-cp ~/xen/xen-4.2.1/extras/mini-os/test.c ~/xen-4.2.1/extras/mini-os
+cp ~/xen/xen-4.2.1-amd64/extras/mini-os/Makefile ~/xen-4.2.1/extras/mini-os
+cp ~/xen/xen-4.2.1-amd64/extras/mini-os/minios.mk ~/xen-4.2.1/extras/mini-os
+cp ~/xen/xen-4.2.1-amd64/extras/mini-os/test.c ~/xen-4.2.1/extras/mini-os
