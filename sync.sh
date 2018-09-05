82545cd06a0a8341814e08c227b22c5054f28e6a
wangchenghku <wangchenghku@gmail.com>
2018-09-05 23:04:24 +0200

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -5,7 +5,9 @@ cp ~/xen/xen-4.2.1-i386/tools/libxc/Makefile ~/xen-4.2.1/tools/libxc
 cp ~/xen/xen-4.2.1-i386/tools/libxc/xc_dom_core.c ~/xen-4.2.1/tools/libxc
 cp ~/xen/xen-4.2.1-i386/tools/libxc/dev_xc_dom_elfdyn.c ~/xen-4.2.1/tools/libxc
 cp ~/xen/xen-4.2.1-i386/tools/libxc/dev_xc_dom_elfdyn.h ~/xen-4.2.1/tools/libxc
+
 cp ~/xen/xen-4.2.1-i386/tools/libxl/xl_cmdimpl.c ~/xen-4.2.1/tools/libxl
+cp ~/xen/xen-4.2.1-i386/extras/mini-os/include/x86/arch_limits.h ~/xen-4.2.1/extras/mini-os/include/x86
 
 cp ~/xen/xen-4.2.1-i386/extras/mini-os/Makefile ~/xen-4.2.1/extras/mini-os
 cp ~/xen/xen-4.2.1-i386/extras/mini-os/minios.mk ~/xen-4.2.1/extras/mini-os
