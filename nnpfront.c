3e6a8adc5ccb1f7a49c441753a8b875bcaa735ae
Cheng Wang2 <nle5255@N-1328.office.hd>
2018-09-19 16:50:10 +0200

    memalign

--- nnpfront.c
+++ nnpfront.c
@@ -95,7 +95,6 @@ void init_nnpfront(void)
    grant_ref = (grant_ref_t*)malloc(sizeof(grant_ref_t) * total_page);
 
    for (i = 0; i < divide_round_up(total_page, 128); ++i) {
-      entry_value;
       snprintf(entry_path, 64, "/local/domain/backend/%d/grant-ref%d", xenbus_get_self_id(), i);
       if((err = xenbus_read(XBT_NIL, entry_path, &entry_value))) {
          NNPFRONT_ERR("Unable to read %s during tpmfront initialization! error = %s\n", entry_path, err);
